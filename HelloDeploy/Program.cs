f5f01b4aea1acafceff37d3322cb844d29f743d7
Paulo Fernando <paulo.fdo@hotmail.com>
2024-04-02 13:55:46 -0300

    Renomeado projeto e arquivos

---
+++ HelloDeploy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+
+namespace HelloDeploy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var apiKey = ConfigurationManager.AppSettings["ApiKey"];
+            Console.WriteLine($"ApiKey: {apiKey}");
+            Console.WriteLine("Pressione uma tecla para Finalizar >>>");
+            Console.ReadKey();
+        }
+    }
+}
