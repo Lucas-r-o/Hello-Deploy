c5bb6f1d5426663e8aed69603314ef17e5a65383
lucas-r-o <lucas.rodrigues@trindadetecnologia.com>
2024-04-02 11:56:01 -0300

    Incluído teste de recuperação do valor do appSettings.

--- POCConfigurationTransformation/Program.cs
+++ POCConfigurationTransformation/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace POCConfigurationTransformation
 {
@@ -10,6 +7,9 @@ namespace POCConfigurationTransformation
     {
         static void Main(string[] args)
         {
+            var value = ConfigurationManager.AppSettings["teste"];
+            Console.WriteLine(value);
+            Console.ReadLine();
         }
     }
 }
